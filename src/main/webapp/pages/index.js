61e7bdb9cfcec98d7adf1a443242745ebebda66d
Biner <545929899@qq.com>
2018-07-01 10:24:28 +0800

    增加github链接

--- src/main/webapp/pages/index.js
+++ src/main/webapp/pages/index.js
@@ -27,6 +27,9 @@ new Vue({
 		logout: function(){
 			var self = this;
 			sessionStorage.removeItem('user', true);
+			// 为了对应canvas加载不出来，需要刷新当前页
+			sessionStorage.setItem('needRefresh', true);
+			this.$router.push("/");
 			// TODO 删除redis
 			alert("退出成功");
 		}
