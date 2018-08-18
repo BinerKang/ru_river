01c22b49c888dc5bdf2e5427e977bcf6bbb331a7
Biner <545929899@qq.com>
2018-08-18 17:33:20 +0800

    完善结构

---
+++ src/main/java/com/biner/ru/service/impl/CommonServiceImpl.java
@@ -0,0 +1,95 @@
+package com.biner.ru.service.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+import com.alibaba.fastjson.JSONObject;
+import com.biner.ru.common.Constants;
+import com.biner.ru.common.MapResult;
+import com.biner.ru.mapper.ScoreMapper;
+import com.biner.ru.model.Score;
+import com.biner.ru.service.CommonService;
+import com.biner.ru.util.AESUtils;
+import com.biner.ru.util.CodeMsg;
+import com.biner.ru.webSocket.SpringWebSocketHandler;
+
+@Service
+public class CommonServiceImpl implements CommonService {
+
+	private Logger logger = Logger.getLogger(this.getClass());
+	// 将排名缓存在内存中，避免每次查询数据库
+	private static Vector<Score> scores = new Vector<Score>();
+	
+	private final static int SCORE_PAGE_SIZE = 5;
+	
+	@Autowired
+	private ScoreMapper scoreMapper;
+	
+	@Bean//这个注解会从Spring容器拿出Bean
+    public SpringWebSocketHandler webSocketHandler() {
+        return new SpringWebSocketHandler();
+    }
+	
+	public MapResult getHomeInfo(String ip) {
+		MapResult result = null;
+		String ipUrl = Constants.FREEGEOIP_URL + ip;
+		String location = "亲爱";
+		Map<String, Object> data = new HashMap<String, Object>();
+		try {
+//			String resultStr = HttpUtils.sendGet(ipUrl);
+//			JSONObject resultJson = JSONObject.parseObject(resultStr);
+//			if ("CN".equals(resultJson.getString("country_code"))) {
+//				String priCode = resultJson.getString("region_code");
+//				location = Constants.PROVINCE_CODE.get(priCode);
+//			}
+		} catch (Exception e) {
+			logger.error("Request Freegeoip has error:::", e);
+			result = new MapResult(CodeMsg.SERVER_EXCEPTION);
+		}
+		if (scores.size() == 0) {
+			Score sc = new Score();
+			sc.setType("1");
+			sc.setPageNo(0);
+			sc.setPageSize(SCORE_PAGE_SIZE);
+			try {
+				scores = scoreMapper.getAllRank(sc);
+			} catch (Exception e) {
+				logger.error("Get all ranks has error:::", e);
+			}
+		}
+		result = new MapResult();
+		data.put("province", location);
+		data.put("scores", scores);
+		result.setData(data);
+		return result;
+	}
+
+	public String getVerifyMailContent(String mail, String username) throws Exception {
+		String token = AESUtils.encrypt(mail + "_" + System.currentTimeMillis(), Constants.SECRET_KEY);
+		String url = Constants.HOST_SERVER_BASE_URL + "#/authMail/" + token;
+		return String.format(Constants.VERIFY_MAIL_CONTENT, username, mail, url);
+	}
+	
+	public MapResult recordScore(String score, String userId) {
+		int scInt = Integer.parseInt(score);
+		Score sc = new Score();
+		sc.setUserId(Long.parseLong(userId));
+		sc.setScore(scInt);
+		sc.setType("1");
+		scoreMapper.save(sc);
+		if (scInt > scores.get(scores.size() - 1).getScore()) {// 推送给前端刷新
+			//TODO 直接操作vector，不从数据库拿
+			sc.setPageNo(0);
+			sc.setPageSize(SCORE_PAGE_SIZE);
+			scores = scoreMapper.getAllRank(sc);
+			webSocketHandler().sendMessageToAll(JSONObject.toJSONString(scores));
+		}
+		return new MapResult();
+	}
+}
---
+++ src/main/java/com/biner/ru/service/impl/UserServiceImpl.java
@@ -0,0 +1,140 @@
+package com.biner.ru.service.impl;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.biner.ru.common.Constants;
+import com.biner.ru.common.MapResult;
+import com.biner.ru.common.RedisKeyConstants;
+import com.biner.ru.mapper.UserMapper;
+import com.biner.ru.model.User;
+import com.biner.ru.service.CommonService;
+import com.biner.ru.service.UserService;
+import com.biner.ru.util.AESUtils;
+import com.biner.ru.util.CodeMsg;
+import com.biner.ru.util.EmailUtils;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	private Logger logger = Logger.getLogger(this.getClass());
+	
+	@Autowired
+	private UserMapper userMapper;
+	
+	@Autowired
+	private CommonService commonService;
+	
+	@Autowired
+	private EmailUtils emailUtils;
+	
+	/**
+	 * 注册用户方法
+	 * @author KangBinbin
+	 * @throws Exception 
+	 * @date 2018-06-03
+	 */
+	public synchronized MapResult register(String sessionId, String username, String password,
+			String mail, String code, String realCode, String score) throws Exception {
+		//TODO 非空校验，参数校验
+		if(!realCode.equalsIgnoreCase(code)) {
+			return new MapResult(CodeMsg.FAIL, "验证码错误");
+		}
+		// 邮箱唯一性校验
+		User u = new User();
+		u.setMail(mail);
+		u = userMapper.selectOne(u);
+		if (u != null) {
+			return new MapResult(CodeMsg.FAIL, "该邮箱已注册");
+		}
+		// 密码加密
+		password = DigestUtils.md5Hex(password);
+		u = new User();
+		u.setPassword(password);
+		u.setMail(mail);
+		u.setUsername(username);
+		// 保存数据库
+		userMapper.save(u);
+		if (!mail.equals(Constants.ADMINSTRATOR_MAIL)) {//不是管理员发送邮件验证
+			// 发送验证邮件
+			String content = commonService.getVerifyMailContent(mail, username);
+			emailUtils.sendEmail(mail, Constants.VERIFY_MAIL_TITLE, content);
+		}
+		// 将session放入redis
+		RedisKeyConstants.addTokenToRedis(sessionId, String.valueOf(u.getId()));
+		if (StringUtils.isNotEmpty(score)) {
+			commonService.recordScore(score, String.valueOf(u.getId()));
+		}
+		return new MapResult();
+	}
+
+	public MapResult login(String sessionId, String password, String mail, String code, 
+			String realCode, String score) throws Exception {
+		//TODO 非空校验，参数校验
+		if(!realCode.equalsIgnoreCase(code)) {
+			return new MapResult(CodeMsg.FAIL, "验证码错误");
+		}
+		// 邮箱唯一性校验
+		User u = new User();
+		u.setMail(mail);
+		u.setPassword(DigestUtils.md5Hex(password));
+		u = userMapper.selectOne(u);
+		if (u == null) {
+			return new MapResult(CodeMsg.FAIL, "账号或密码错误");
+		}
+		// 将session放入redis
+		RedisKeyConstants.addTokenToRedis(sessionId, String.valueOf(u.getId()));
+    	if (StringUtils.isNotEmpty(score)) {
+			commonService.recordScore(score, String.valueOf(u.getId()));
+		}
+		MapResult result = new MapResult();
+		Map<String, Object> data = new HashMap<String, Object>();
+		data.put("userInfo", u);
+		result.setData(data);
+		return result;
+	}
+	
+	@Transactional
+	public MapResult authMail(String sessionId, String code) throws Exception {
+		if(StringUtils.isEmpty(code)) {
+			return new MapResult(CodeMsg.FAIL, "参数为空");
+		}
+		code = AESUtils.decrypt(code, Constants.SECRET_KEY);
+		if (!code.contains("_")) {
+			return new MapResult(CodeMsg.FAIL, "url异常");
+		}
+		String[] info = code.split("_");
+		double day = (System.currentTimeMillis() - Long.parseLong(info[1])) / (1000 * 60 * 60 * 24.0);
+		if (day > Constants.VERIFY_MAIL_EXPIRE_DAYS) {
+			return new MapResult(CodeMsg.FAIL, "Sorry，该邮件链接已失效");
+		}
+		User u = new User();
+		u.setMail(info[0]);
+		u.setValidate(true);
+		int count = userMapper.updateByMail(u);
+		if (count != 1) {
+			throw new Exception("Auth Mail update result count is not one.");
+		} else {
+			u = userMapper.selectOne(u);
+		}
+		// 将session放入redis
+		RedisKeyConstants.addTokenToRedis(sessionId, String.valueOf(u.getId()));
+		MapResult result = new MapResult();
+		Map<String, Object> data = new HashMap<String, Object>();
+		data.put("userInfo", u);
+		result.setData(data);
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(DigestUtils.md5Hex("123asd"));
+	}
+}
